3016455c540c34bbb07334a1947ccd0ecbff816b
Alexandr Baranov <alexbrnov@gmail.com>
2023-12-17 20:27:34 +0300

    Initial Commit

---
+++ SlidersSwiftUI/SlidersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersSwiftUIApp.swift
+//  SlidersSwiftUI
+//
+//  Created by Alexandr Baranov on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SlidersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
